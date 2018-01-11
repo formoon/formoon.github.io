a407c2b30d0be5238b0b1f02e7aa6c4935c94561
AndrewWang <andrewwang@sina.com>
2018-01-11 14:25:31 +0800

    add post ml-nn

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git add . 
-git commit -m "$1"
-git push
-if [ "$2" = "upload" ]; then
+if [ "$1" = "pic" ]; then
 	qshell qupload 8 qshellupload.conf
+else
+	git add . 
+	git commit -m "$1"
+	git push
 fi
 
